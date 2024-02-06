39ea438c3c0a6f22b97e94f8b85a1c4d1254d22a
ankitbhandari <newmew.ankit@gmail.com>
2024-02-06 23:04:33 +0545

    next router

---
+++ src/app/Showcase2/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Index = (props: Props) => {
+  return (
+    <div>
+      <div className="w-screen h-screen bg-blue-300">Showcase 2</div>
+    </div>
+  );
+};
+
+export default Index;
