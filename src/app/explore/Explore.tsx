20c4996027318bacceb2cb3afee2b03e2c8b88cf
adarshkunwar <hellboyadarsh18@gmail.com>
2024-02-13 16:01:52 +0545

    merge

--- src/app/explore/Explore.tsx
+++ src/app/explore/Explore.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Explore = (props: Props) => {
   return (
     <div className="text-white">
-      E x p l o r e &nbsp;&nbsp;&nbsp; p a g e &nbsp;&nbsp;&nbsp; h e r e
+      E x p l o r e &nbsp;&nbsp;&nbsp; p a g e &nbsp;&nbsp;&nbsp; h e r
     </div>
   );
 };
