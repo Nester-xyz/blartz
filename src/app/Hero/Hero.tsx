ad42724b73f06bfbee67f8b1aaed79de0121199a
adarshkunwar <hellboyadarsh18@gmail.com>
2024-02-13 20:47:11 +0545

    ui is aligned now

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -8,6 +8,20 @@ import CustomButton from "@/Components/UI/CustomButton";
 type Props = {};
 
 const Index = (props: Props) => {
+  const card = (name: string, position: string) => {
+    return (
+      <div className="flex items-center px-3 md:px-5 py-2 gap-2 border border-primary rounded-lg">
+        <div className="w-10 h-10 bg-primary rounded-full flex-shrink-0">
+          <Image className="rounded-full" src={defaultImg} alt={name}></Image>
+        </div>
+        <div className="flex flex-col text-primary">
+          <div className="text-xl">{name}</div>
+          <div className="text-sm">{position}</div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="">
       <div className="w-10/12 mx-auto h-fit md:h-screen bg-black pb-20 grid md:grid-rows-5 text-primary">
@@ -15,15 +29,15 @@ const Index = (props: Props) => {
           <div className="col-span-2  bg-black grid items-center">
             <div className="text-left flex flex-col gap-5">
               <h1 className="text-5xl">Blartz ⚡</h1>
-              <p className="text-lg text-primary">
+              <p className="text-lg text-primary break-all">
                 The NFT marketplace you have ever wanted.
               </p>
-              <div>
+              <div className="mx-auto md:mx-0">
                 <CustomButton text="Explore" link="/explore" />
               </div>
             </div>
           </div>
-          <div className="grid justify-end items-center">
+          <div className="grid justify-center md:justify-end items-center">
             <div className="w-80 h-80 md:w-96 md:h-96 border border-primary shadow-2xl rounded-xl ">
               <Image
                 src={mainImg}
@@ -33,49 +47,13 @@ const Index = (props: Props) => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-4 mx-auto h-fit md:w-full px-0  pt-10">
+        <div className="flex flex-col gap-4  h-fit md:w-full px-0  pt-10">
           <div className="text-3xl text-primary">Team</div>
           <div className="tex-md">
-            <div className="flex flex-col md:grid md:grid-cols-3 gap-5">
-              <div className="flex items-center px-3 md:px-5 py-2 gap-2 border border-primary rounded-lg">
-                <div className="w-10 h-10 bg-primary rounded-full flex-shrink-0">
-                  <Image
-                    className="rounded-full"
-                    src={defaultImg}
-                    alt="slave"
-                  ></Image>
-                </div>
-                <div className="flex flex-col text-primary">
-                  <div className="text-xl">Alex commoner</div>
-                  <div className="text-sm">Co-founder, Frontend</div>
-                </div>
-              </div>
-              <div className="flex items-center px-3 md:px-5 py-2 gap-2 border border-primary rounded-lg">
-                <div className="w-10 h-10 bg-primary rounded-full flex-shrink-0">
-                  <Image
-                    className="rounded-full"
-                    src={defaultImg}
-                    alt="slave"
-                  ></Image>
-                </div>
-                <div className="flex flex-col text-primary">
-                  <div className="text-xl">Aryog</div>
-                  <div className="text-sm">Co-founder, Smart Contract </div>
-                </div>
-              </div>
-              <div className="flex items-center px-3 md:px-5 py-2 gap-2 border border-primary rounded-lg">
-                <div className="w-10 h-10 bg-primary rounded-full flex-shrink-0">
-                  <Image
-                    className="rounded-full"
-                    src={defaultImg}
-                    alt="slave"
-                  ></Image>
-                </div>
-                <div className="flex flex-col text-primary">
-                  <div className="text-xl">Anku</div>
-                  <div className="text-sm">Co founder, Full-Stack</div>
-                </div>
-              </div>
+            <div className="flex  flex-col md:grid md:grid-cols-3 gap-5">
+              {card("Alex commoner", "Co-founder, Frontend")}
+              {card("Aryog", "Co-founder, Smart Contract")}
+              {card("Anku", "Co founder, Full-Stack")}
             </div>
           </div>
         </div>
