8c88dc3c4d260051faceef8e0ab13e4d5e4d842b
ankitbhandari <newmew.ankit@gmail.com>
2024-02-13 16:45:49 +0545

    compiler problem detected

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -5,7 +5,13 @@ type Props = {
 };
 
 const Layout = ({ children }: Props) => {
-  return <div className="text-white w-full h-full bg-red-400"></div>;
+  return (
+    <div className="text-white w-full h-screen grid grid-cols-12 bg-red-400">
+      <div className="col-span-2 bg-green-500 h-full"></div>
+      <div className="col-span-8 bg-yellow-700 h-full">{children}</div>
+      <div className="col-span-2 bg-blue-800 h-full"></div>
+    </div>
+  );
 };
 
 export default Layout;
