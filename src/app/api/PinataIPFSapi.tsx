3458480b4b34b761e10450ad1b0b4c6539eb01ca
Aryog <yogesh000aryal@gmail.com>
2024-02-14 12:12:50 +0545

    ipfs upload added

---
+++ src/app/api/PinataIPFSapi.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+// require('dotenv').config()
+const pinFileToIPFS = async (file: File) => {
+    if (!file.type.startsWith('image/')) {
+        console.error('Only images are supported for Upload');
+        return "Error File Type";
+    }
+    const formData = new FormData();
+    formData.append('file', file)
+
+    const pinataMetadata = JSON.stringify({
+        name: file.name,
+    });
+    formData.append('pinataMetadata', pinataMetadata);
+
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+    })
+    formData.append('pinataOptions', pinataOptions);
+    console.log(process.env.NEXT_PUBLIC_PINATA_IPFS_JWT)
+    try {
+        const formDataAny: any = formData;
+        const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+            maxBodyLength: Infinity,
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${formDataAny._boundary}`,
+                'Authorization': `Bearer ${process.env.NEXT_PUBLIC_PINATA_IPFS_JWT}`
+            }
+        });
+        console.log(res.data);
+        return res.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default pinFileToIPFS;
